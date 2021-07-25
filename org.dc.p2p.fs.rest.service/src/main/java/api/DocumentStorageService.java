cff253b9d5dac659c980acdb0f7bdb6bac62f2ad
Dumindu-Kanchana <dkgeeganage@gmail.com>
2021-07-25 17:43:54 +0530

    print md5sum of the sent file in the server

--- org.dc.p2p.fs.rest.service/src/main/java/api/DocumentStorageService.java
+++ org.dc.p2p.fs.rest.service/src/main/java/api/DocumentStorageService.java
@@ -1,21 +1,28 @@
 package api;
 
 import conf.ServerConfigurations;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 
+import javax.xml.bind.DatatypeConverter;
 import java.io.File;
 import java.io.RandomAccessFile;
 import java.net.MalformedURLException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.MessageDigest;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
+import static java.nio.file.Files.readAllBytes;
 
 @Service
 public class DocumentStorageService {
+
+    private static final Logger log = LoggerFactory.getLogger(DocumentStorageService.class);
 
     public Resource getFileAsResource(String name) throws MalformedURLException {
         File file = new File(ServerConfigurations.props.getProperty("fileDownload.tempDir") + "/" + name);
@@ -30,9 +37,23 @@ public class DocumentStorageService {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        Path filePath = Paths.get(ServerConfigurations.props.getProperty("fileDownload.tempDir")).toAbsolutePath().normalize();
-        Path targetLocation = filePath.resolve(name);
-        return new UrlResource(targetLocation.toUri());
+        Path DirectoryPath = Paths.get(ServerConfigurations.props.getProperty("fileDownload.tempDir")).toAbsolutePath().normalize();
+        printMD5ofFile(DirectoryPath.resolve(name), name);
+        return new UrlResource( DirectoryPath.resolve(name).toUri());
 
+    }
+
+    private static void printMD5ofFile(Path filePath, String name) {
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("MD5");
+            md.update(readAllBytes(filePath));
+        } catch (Exception e) {
+            log.error("Error while calculating MD5sum of " + name + " file.");
+            e.printStackTrace();
+        }
+        byte[] digest = md.digest();
+        String myChecksum = DatatypeConverter.printHexBinary(digest).toUpperCase();
+        log.info("MD5sum of the file " + name + " is " + myChecksum);
     }
 }
