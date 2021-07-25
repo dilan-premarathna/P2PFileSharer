d9140675abd7271974f2f2593037097eb75cfd08
Shanaka <premarathna.shanaka.dilan@gmail.com>
2021-07-25 15:07:35 +0530

    register with bs

--- org.dc.p2p.fs.server/src/main/java/service/Neighbour.java
+++ org.dc.p2p.fs.server/src/main/java/service/Neighbour.java
@@ -1,9 +1,13 @@
 package service;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.List;
+
 class Neighbour {
     private String ip;
     private int port;
-    private String username;
 
     public Neighbour(String ip, int port){
         this.ip = ip;
@@ -17,4 +21,49 @@ class Neighbour {
     public int getPort(){
         return this.port;
     }
+
+    public boolean NeighbourConnect(List<Neighbour> neighbourList)  throws Exception {
+
+        String joinMessage ="0027 JOIN 64.12.123.190 432";
+        InetAddress ia = InetAddress.getByName(getIp());
+        byte[] messageBytes = joinMessage.getBytes();
+        DatagramPacket packet = new DatagramPacket(messageBytes, messageBytes.length, ia, getPort());
+
+        DatagramSocket socket = new DatagramSocket();
+        socket.send(packet);
+
+        byte[] b1 = new byte[2048];
+        DatagramPacket dpResponse = new DatagramPacket(b1, b1.length);
+        socket.receive(dpResponse);
+
+        String nodeResponse = new String(dpResponse.getData());
+        System.out.println("result is " + nodeResponse);
+        socket.close();
+        return processNodeResponse(nodeResponse.trim(),neighbourList);
+    }
+
+    public boolean processNodeResponse(String message, List<Neighbour> neighbourList)
+            throws Exception {
+
+        String[] mes = message.split(" ");
+        System.out.println(message);
+        System.out.println("processNodeResponse: " + mes[2]);
+
+
+        if (mes[1].equals("JOINOK")) {
+            if (mes[2].equals("0")) {
+                for (Neighbour neb:neighbourList) {
+                    if(neb.getIp() == this.getIp() && neb.getPort() == this.getPort())
+                        return false;
+                }
+                neighbourList.add(this);
+
+            } else if (mes[2].equals("9999")) {
+                throw new Exception("failed, can’t register." + " initial join failed");
+            }
+        }
+        return true;
+    }
+
+
 }
--- org.dc.p2p.fs.server/src/main/java/service/Node.java
+++ org.dc.p2p.fs.server/src/main/java/service/Node.java
@@ -2,23 +2,69 @@ package service;
 
 import util.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node {
 
     private String name;
-    private String bsServerIP="127.0.0.1";
-    private int bsServerPort=55555;
-    private String clientPort;
-    private Service service =new Service();
+    private String bsServerIP = "127.0.0.1";
+    private int bsServerPort = 55555;
+    private Service service = new Service();
+    private final List<Neighbour> neighboursList = new ArrayList<>();
 
     public void registerNode() throws Exception {
-        Neighbour[] neighbours = null;
-        String regMessage ="0036 REG 129.82.123.45 5004 1234abcdk";
-        service.sendToBS(regMessage,bsServerIP,bsServerPort);
+
+        String regMessage = "0036 REG 127.0.0.1 5000 client1";
+        String bsResponse = service.sendToBS(regMessage, bsServerIP, bsServerPort);
+        Neighbour[] neighbours = processBSResponse(bsResponse.trim());
+        if (neighbours.length != 0) {
+            processNeighbour(neighbours);
+        }
+        System.out.println(neighbours);
     }
 
     public void unRegisterNode() throws Exception {
+
         Neighbour[] neighbours = null;
-        String regMessage ="0036 REG 129.82.123.45 5001 1234abcd";
-        service.sendToBS(regMessage,bsServerIP,bsServerPort);
+        String regMessage = "0036 REG 129.82.123.45 5001 1234abcd";
+        service.sendToBS(regMessage, bsServerIP, bsServerPort);
+    }
+
+    public Neighbour[] processBSResponse(String message)
+            throws Exception {
+
+        String[] mes = message.split(" ");
+        System.out.println(message);
+        System.out.println("processBSResponse: " + mes[2]);
+        int noNodes = Integer.parseInt(mes[2]);
+
+        if (0 <= noNodes && noNodes <= 2) {
+            Neighbour[] neighbour = new Neighbour[noNodes];
+            for (int n = 0; n < noNodes; n++) {
+                neighbour[n] = new Neighbour(mes[(n * 2) + 3],
+                        Integer.parseInt(mes[(n * 2) + 4]));
+            }
+            return neighbour;
+        } else {
+            switch (noNodes) {
+                case 9996:
+                    throw new Exception("failed, can’t register." + " BS full");
+                case 9997:
+                    throw new Exception("failed, registered" + " to another user, try a different IP and port");
+                case 9998:
+                    throw new Exception("failed, already registered" + " to you, unregister first");
+                case 9999:
+                    throw new Exception("failed, there is some error" + " in the command");
+            }
+        }
+        return null;
+    }
+
+    public void processNeighbour(Neighbour[] neighbours) throws Exception {
+
+        for (Neighbour neighbour : neighbours) {
+            neighbour.NeighbourConnect(neighboursList);
+        }
     }
 }
